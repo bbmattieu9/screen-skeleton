c276cdf7135c80428298c4650e78534af8e23c2c
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-09-21 15:17:28 +0100

    [Skeleton] Add preloader to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SkeltonModule } from './screen-loader/skelton/skelton.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SkeltonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
